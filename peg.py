ce11db25b3afb295ada9e20e7d1284be99d5228d
Joshua Morton <joshua.morton@gatech.edu>
2016-04-27 01:55:58 -0400

    Now less cruft

--- peg.py
+++ peg.py
@@ -95,7 +95,9 @@ class NotError(GrammarError):
 class P:
     def __init__(self, grammar):
         self.grammar = grammar
-        self.parsed_grammar = self._grammar(grammar)
+        self.syntax_tree, _ = self._grammar(grammar)
+        print(self.syntax_tree)
+
 
     @staticmethod
     def _grammar(grammar):
@@ -408,9 +410,8 @@ class P:
                 return [], rest
             raise GrammarError
         return inner
+
 p1 = P(gram)
-
-
 
 
 
